d0d872985ca0528d139957a67dbb685bd118e20d
daida.medina <daida.medina@codurance.com>
2023-10-04 15:36:47 +0100

    Add a test case

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,6 +6,7 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
+      new Item("Aged Brie", -2, 0),
       new Item("Elixir of the Mongoose", 5, 7),
       new Item("Sulfuras, Hand of Ragnaros", 0, 80),
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
